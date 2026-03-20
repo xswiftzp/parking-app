e60a970f81900a06fb5cc2977f01b356c2ad3137
xswiftzp <xswiftzp@gmail.com>
2026-03-20 20:32:40 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open('app').then(c=>c.addAll(['./','./index.html']))
+  );
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
